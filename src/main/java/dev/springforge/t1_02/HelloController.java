d2f42adea6beadf49a36b644409c883a068c2a70
LAPTOP-IKQ1FNCH\Mark Paul <markbaddev@gmail.com>
2026-03-26 20:06:45 +0800

    stub controller for t1-02 exercises

--- src/main/java/dev/springforge/t1_02/HelloController.java
+++ src/main/java/dev/springforge/t1_02/HelloController.java
@@ -1,19 +1,29 @@
 package dev.springforge.t1_02;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
+/**
+ * Exercise 1 & 2: Your First REST Controller
+ *
+ * YOUR TASKS:
+ *
+ * Ex01: Create a GET endpoint at "/hello" that returns "Hello, SpringForge!"
+ *   Hint: You need @RestController on the class and @GetMapping("/hello") on the method.
+ *
+ * Ex02: Create a GET endpoint at "/hello/{name}" that returns "Hello, {name}!"
+ *   Hint: Use @PathVariable to capture the {name} from the URL.
+ */
+// TODO: Add @RestController annotation
 public class HelloController {
 
-    @GetMapping("/hello")
+    // TODO: Add @GetMapping("/hello")
     public String hello() {
-        return "Hello, SpringForge!";
+        // TODO: Return "Hello, SpringForge!"
+        throw new UnsupportedOperationException("Implement hello()");
     }
 
-    @GetMapping("/hello/{name}")
-    public String helloName(@PathVariable String name) {
-        return "Hello, " + name + "!";
+    // TODO: Add @GetMapping("/hello/{name}")
+    public String helloName(String name) {
+        // TODO: Add @PathVariable annotation to name parameter
+        // TODO: Return "Hello, " + name + "!"
+        throw new UnsupportedOperationException("Implement helloName()");
     }
 }
